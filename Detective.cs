efcddd269528c341c3f6e7b11723b2e2691a6de2
carlz54339 <chep3490@gmail.com>
2025-09-30 17:02:07 -0400

    Add files via upload

---
+++ Detective.cs
@@ -0,0 +1,300 @@
+﻿using Il2Cpp;
+using Il2CppFIMSpace.Basics;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppInterop.Runtime.InteropTypes;
+using Il2CppSystem;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using UnityEngine;
+using CarlzVilliagePack;
+using System.ComponentModel.Design;
+using static Il2CppSystem.Globalization.CultureInfo;
+using static MelonLoader.MelonLaunchOptions;
+
+namespace CarlzVilliagePack;
+[RegisterTypeInIl2Cpp]
+public class Detective : Role
+{
+    Character chRef;
+    private Il2CppSystem.Action actionCP;
+    private Il2CppSystem.Action actionSP;
+    private Il2CppSystem.Action actionCPD;
+    public override string Description
+    {
+        get
+        {
+            return "";
+        }
+    }
+
+    public override ActedInfo bcq(Character charRef)
+    {
+        return new ActedInfo("", null);
+    }
+
+    public override ActedInfo bcr(Character charRef)
+    {
+        return new ActedInfo("", null);
+    }
+
+    public override void bcs(ETriggerPhase trigger, Character charRef)
+    {
+        if (trigger != ETriggerPhase.Day) return;
+        chRef = charRef;
+        CharacterPicker.Instance.kx(3);
+        CharacterPicker.OnCharactersPicked += actionCP;
+        CharacterPicker.OnStopPick += actionSP;
+    }
+
+    public override void bcx(ETriggerPhase trigger, Character charRef)
+    {
+        if (trigger != ETriggerPhase.Day) return;
+        chRef = charRef;
+        CharacterPicker.Instance.kx(3);
+        CharacterPicker.OnCharactersPicked += actionCPD;
+        CharacterPicker.OnStopPick += actionSP;
+    }
+
+    private void StopPick()
+    {
+        CharacterPicker.OnCharactersPicked -= actionCP;
+        CharacterPicker.OnStopPick -= actionSP;
+        CharacterPicker.OnCharactersPicked -= actionCPD;
+    }
+
+    private void CharacterPicked()
+    {
+        CharacterPicker.OnCharactersPicked -= actionCP;
+        CharacterPicker.OnStopPick -= actionSP;
+
+        int villagers = 0;
+        int outcasts = 0;
+        int minions = 0;
+        int demons = 0;
+
+        Il2CppSystem.Collections.Generic.List<int> ids = new Il2CppSystem.Collections.Generic.List<int>();
+        Il2CppSystem.Collections.Generic.List<Character> chars = new Il2CppSystem.Collections.Generic.List<Character>();
+        foreach (Character c in CharacterPicker.PickedCharacters)
+        {
+            if (c.dl().type == ECharacterType.Villager)
+                villagers++;
+            else if (c.dl().type == ECharacterType.Outcast || c.dq().characterId == "Wretch_80988916")
+                outcasts++;
+            else if (c.dl().type == ECharacterType.Minion)
+                minions++;
+            else
+                demons++;
+
+            ids.Add(c.id);
+            chars.Add(c);
+        }
+
+        string info = $"Among #{ids[0]}, #{ids[1]}, #{ids[2]}\nthere are: ";
+
+        if (villagers == 0)
+            info = info + $"";
+        else if (villagers == 1)
+            info = info + $"{villagers} villager";
+        else
+            info = info + $"{villagers} villagers";
+
+        if (outcasts == 0)
+            info = info + $"";
+        else if(outcasts == 1 && villagers == 0)
+            info = info + $"{outcasts} outcast";
+        else if (outcasts == 1)
+            info = info + $", {outcasts} outcast";
+        else if (outcasts == 2 && villagers == 0)
+            info = info + $"{outcasts} outcasts";
+        else if (outcasts == 2)
+            info = info + $", {outcasts} outcasts";
+        else
+            info = info + $"{outcasts} outcasts";
+
+        if (minions == 0)
+            info = info + $"";
+        else if(minions == 1 && villagers == 0 && outcasts == 0)
+            info = info + $"{minions} minion";
+        else if (minions == 1)
+            info = info + $", {minions} minion";
+        else if (minions == 2 && villagers == 0 && outcasts == 0)
+            info = info + $"{minions} minions";
+        else if (minions == 2)
+            info = info + $", {minions} minions";
+        else
+            info = info + $"{minions} minions";
+
+        if (demons == 0)
+            info = info + $"";
+        else if (demons == 1)
+            info = info + $", {demons} demon";
+        else if (demons == 2)
+            info = info + $", {demons} demons";
+        else
+            info = info + $"{demons} demons";
+
+        onActed?.Invoke(new ActedInfo(info, chars));
+        Debug.Log($"{info}");
+    }
+
+    private void CharacterPickedDrunk()
+    {
+        CharacterPicker.OnCharactersPicked -= actionCPD;
+        CharacterPicker.OnStopPick -= actionSP;
+
+        int villagers = 0;
+        int outcasts = 0;
+        int minions = 0;
+        int demons = 0;
+        int fakeVC = 0;
+        int fakeOC = 0;
+        int fakeMC = 0;
+        int fakeDC = 0;
+
+        int rng = 0;
+
+        Il2CppSystem.Collections.Generic.List<int> ids = new Il2CppSystem.Collections.Generic.List<int>();
+        Il2CppSystem.Collections.Generic.List<Character> chars = new Il2CppSystem.Collections.Generic.List<Character>();
+        foreach (Character c in CharacterPicker.PickedCharacters)
+        {
+            if (c.dl().type == ECharacterType.Villager)
+                demons++;
+            else if (c.dl().type == ECharacterType.Outcast || c.dq().characterId == "Wretch_80988916")
+                minions++;
+            else if (c.dl().type == ECharacterType.Minion)
+                villagers++;
+            else
+                outcasts++;
+
+            rng = UnityEngine.Random.Range(0, 4);
+
+            if (rng == 0)
+                fakeVC++;
+            else if (rng == 1)
+                fakeOC++;
+            else if (rng == 2)
+                fakeMC++;
+            else
+                fakeDC++;
+            
+            ids.Add(c.id);
+            chars.Add(c);
+        }
+
+        if (villagers == fakeVC && outcasts == fakeOC && minions == fakeMC && demons == fakeDC)
+        {
+            if(villagers != 0)
+            {
+                fakeVC--;
+                rng = UnityEngine.Random.Range(0, 3);
+                if (rng == 0)
+                    fakeOC++;
+                else if (rng == 1)
+                    fakeMC++;
+                else
+                    fakeDC++; 
+            }
+            else if(outcasts != 0)
+            {
+                fakeOC--;
+                rng = UnityEngine.Random.Range(0, 3);
+                if (rng == 0)
+                    fakeVC++;
+                else if (rng == 1)
+                    fakeMC++;
+                else
+                    fakeDC++;
+            }
+            else if(minions != 0)
+            {
+                fakeMC--;
+                rng = UnityEngine.Random.Range(0, 3);
+                if (rng == 0)
+                    fakeVC++;
+                else if (rng == 1)
+                    fakeOC++;
+                else
+                    fakeDC++;
+            }
+            else
+            {
+                fakeDC--;
+                rng = UnityEngine.Random.Range(0, 3);
+                if (rng == 0)
+                    fakeVC++;
+                else if (rng == 1)
+                    fakeOC++;
+                else
+                    fakeMC++;
+            }
+        }
+
+        string info = $"Among #{ids[0]}, #{ids[1]}, #{ids[2]}\nthere are: ";
+
+        if (villagers == 0)
+            info = info + $"";
+        else if (villagers == 1)
+            info = info + $"{villagers} villager";
+        else
+            info = info + $"{villagers} villagers";
+
+        if (outcasts == 0)
+            info = info + $"";
+        else if (outcasts == 1 && villagers == 0)
+            info = info + $"{outcasts} outcast";
+        else if (outcasts == 1)
+            info = info + $", {outcasts} outcast";
+        else if (outcasts == 2 && villagers == 0)
+            info = info + $"{outcasts} outcasts";
+        else if (outcasts == 2)
+            info = info + $", {outcasts} outcasts";
+        else
+            info = info + $"{outcasts} outcasts";
+
+        if (minions == 0)
+            info = info + $"";
+        else if (minions == 1 && villagers == 0 && outcasts == 0)
+            info = info + $"{minions} minion";
+        else if (minions == 1)
+            info = info + $", {minions} minion";
+        else if (minions == 2 && villagers == 0 && outcasts == 0)
+            info = info + $"{minions} minions";
+        else if (minions == 2)
+            info = info + $", {minions} minions";
+        else
+            info = info + $"{minions} minions";
+
+        if (demons == 0)
+            info = info + $"";
+        else if (demons == 1)
+            info = info + $", {demons} demon";
+        else if (demons == 2)
+            info = info + $", {demons} demons";
+        else
+            info = info + $"{demons} demons";
+
+        onActed?.Invoke(new ActedInfo(info, chars));
+        Debug.Log($"{info}");
+    }
+
+    public Detective() : base(ClassInjector.DerivedConstructorPointer<Detective>())
+    {
+        ClassInjector.DerivedConstructorBody((Il2CppObjectBase)this);
+        actionCP = new System.Action(CharacterPicked);
+        actionSP = new System.Action(StopPick);
+        actionCPD = new System.Action(CharacterPickedDrunk);
+    }
+
+    public Detective(System.IntPtr ptr) : base(ptr)
+    {
+        actionCP = new System.Action(CharacterPicked);
+        actionSP = new System.Action(StopPick);
+        actionCPD = new System.Action(CharacterPickedDrunk);
+    }
+}
+
---
+++ Good_Twin.cs
@@ -0,0 +1,122 @@
+﻿using Il2Cpp;
+using Il2CppFIMSpace.Basics;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppInterop.Runtime.InteropTypes;
+using Il2CppSystem;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using UnityEngine;
+using CarlzVilliagePack;
+using static Il2CppSystem.Globalization.CultureInfo;
+using static MelonLoader.MelonLaunchOptions;
+
+namespace CarlzVilliagePack;
+[RegisterTypeInIl2Cpp]
+public class Good_Twin : Role
+{
+    public CharacterData[] allDatas = Il2CppSystem.Array.Empty<CharacterData>();
+    public override string Description
+    {
+        get
+        {
+            return "";
+        }
+    }
+
+    public override ActedInfo bcq(Character charRef)
+    {
+        return new ActedInfo("", null);
+    }
+
+    public override ActedInfo bcr(Character charRef)
+    {
+        return new ActedInfo("", null);
+    }
+
+    public override void bcs(ETriggerPhase trigger, Character charRef)
+    {
+        if(trigger == ETriggerPhase.Start)
+        {
+            charRef.statuses.fm(ECharacterStatus.UnkillableByDemon, charRef);
+            Il2CppSystem.Collections.Generic.List<Character> chars = new Il2CppSystem.Collections.Generic.List<Character>(Gameplay.CurrentCharacters.Pointer);
+            Character pickedChar = new Character();
+            chars = Characters.Instance.gq(chars, ECharacterType.Villager);
+            chars = Characters.Instance.gx(chars, EAlignment.Good);
+            pickedChar = chars[UnityEngine.Random.Range(0, chars.Count - 1)];
+            foreach(Character c in Gameplay.CurrentCharacters)
+            {
+                if(c == pickedChar)
+                {
+                    if (allDatas.Length == 0)
+                    {
+                        var loadedCharList = Resources.FindObjectsOfTypeAll(Il2CppType.Of<CharacterData>());
+                        if (loadedCharList != null)
+                        {
+                            allDatas = new CharacterData[loadedCharList.Length];
+                            for (int i = 0; i < loadedCharList.Length; i++)
+                            {
+                                allDatas[i] = loadedCharList[i]!.Cast<CharacterData>();
+                            }
+                        }
+                    }
+                    
+                    for (int i = 0; i < allDatas.Length; i++)
+                    {
+                        if (allDatas[i].characterId == "EvilTwin_VP")
+                        {
+                            if (c.dl().characterId != allDatas[i].characterId)
+                            {
+                                c.dv(allDatas[i]);
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        if (trigger != ETriggerPhase.Day) return;
+    }
+
+    public override void bct(Character charRef)
+    {
+        if(charRef.killedByDemon == false )
+            PlayerController.PlayerInfo.health.jl(3);
+    }
+
+    public override CharacterData bcz(Character charRef)
+    {
+        charRef.statuses.fm(ECharacterStatus.HealthyBluff, charRef);
+        foreach (Character c in Gameplay.CurrentCharacters)
+        {
+            if (c.dl().characterId == "EvilTwin_VP")
+            {
+                if (c.bluff != null)
+                    return c.bluff;
+                else
+                {
+                    CharacterData bluff = Characters.Instance.gd();
+                    Gameplay.Instance.mm(bluff.type, bluff);
+                    return bluff;
+                }
+            }
+        }
+        return null; //used in case something is missed
+    }
+
+    public Good_Twin() : base(ClassInjector.DerivedConstructorPointer<Good_Twin>())
+    {
+        ClassInjector.DerivedConstructorBody((Il2CppObjectBase)this);
+
+    }
+
+    public Good_Twin(System.IntPtr ptr) : base(ptr)
+    {
+
+    }
+}
